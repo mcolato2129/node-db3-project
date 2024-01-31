bed1ee8fea1f8034acc15cd6474240e42f541283
Marlon Colato <mcolato2104@gmail.com>
2024-01-31 18:38:41 -0500

    h

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -118,7 +118,7 @@ async function findById(scheme_id) { // EXERCISE B
 
 }
 
-function findSteps(scheme_id) { // EXERCISE C
+async function findSteps(scheme_id) { // EXERCISE C
   /*
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
@@ -146,11 +146,14 @@ function findSteps(scheme_id) { // EXERCISE C
     WHERE sc.scheme_id = 1
     ORDER BY step_number
   */
-  return db('schemes as sc')
+  const rows = await db('schemes as sc')
   .leftJoin('steps as st', 'sc.scheme_id', '=', 'st.scheme_id')
   .where('sc.scheme_id', scheme_id)
   .select('step_id', 'step_number', 'instructions', 'scheme_name')
   .orderBy('step_number')
+
+    if(!rows[0].step_id) return []
+    return  rows
 
 }
 
